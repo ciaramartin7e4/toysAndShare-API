9b79be81c4a3b218d37efc6610bbf490728a1bb2
sjo <joseph.duran.7e4@itb.cat>
2022-02-25 15:44:55 +0100

    API TOYSANDSHARE

--- src/main/java/cat/itb/m13/toysandsahre/controladors/ControladorToysAndShare.java
+++ src/main/java/cat/itb/m13/toysandsahre/controladors/ControladorToysAndShare.java
@@ -1,15 +1,15 @@
 package cat.itb.m13.toysandsahre.controladors;
 
 import cat.itb.m13.toysandsahre.model.entitats.GoogleUsers;
+import cat.itb.m13.toysandsahre.model.entitats.Products;
 import cat.itb.m13.toysandsahre.model.entitats.Users;
 import cat.itb.m13.toysandsahre.model.repositoris.ServeisGoogle;
 import cat.itb.m13.toysandsahre.model.serveis.ServeisProduct;
 import cat.itb.m13.toysandsahre.model.serveis.ServeisUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.PrivateKey;
 import java.util.List;
@@ -36,6 +36,54 @@ public class ControladorToysAndShare {
         Users user = serveisUser.getById(id);
         if (user == null) return ResponseEntity.notFound().build();
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Users> postUser(@RequestBody Users user) {
+        Users u = serveisUser.set(user);
+        return new ResponseEntity<Users>(u, HttpStatus.CREATED);
+    }
+    @PutMapping("/users")
+    public ResponseEntity<Users> updateUser(@RequestBody Users user) {
+        Users u = serveisUser.set(user);
+        return ResponseEntity.ok(u);
+    }
+    @DeleteMapping("/users/{id}")
+    public Users deleteUser(@PathVariable int id) {
+        return serveisUser.delete(id);
+    }
+
+    //PRODUCTS
+    @GetMapping("/products")
+    public ResponseEntity<List<Products>> getLists() {
+        return ResponseEntity.ok(serveisProduct.get());
+    }
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Products> getListsById(@PathVariable int id){
+        Products products = serveisProduct.getById(id);
+        if(products == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(products);
+    }
+    @PostMapping("/products")
+    public ResponseEntity<Products> postLista(@RequestBody Products products){
+        Products p = serveisProduct.set(products);
+        return new ResponseEntity<Products>(p, HttpStatus.CREATED);
+    }
+    @PutMapping("/products")
+    public ResponseEntity<Products> updateLista(@RequestBody Products lista) {
+        Products p = serveisProduct.set(lista);
+        return ResponseEntity.ok(p);
+    }
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Integer> deleteList(@PathVariable int id) {
+        serveisProduct.delete(id);
+        return new ResponseEntity<>(id, HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/products/user/{id}")
+    public ResponseEntity<List<Products>> getListByUserId(@PathVariable int id) {
+        List<Products> lista = serveisProduct.getListByUserId(id);
+        return ResponseEntity.ok(lista);
     }
 
 }
