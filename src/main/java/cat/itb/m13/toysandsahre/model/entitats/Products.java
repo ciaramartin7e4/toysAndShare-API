19a75acd5c74d272d61e5947d3b1cde702dbadb8
sjo <joseph.duran.7e4@itb.cat>
2022-02-25 15:54:55 +0100

    API TOYSANDSHARE

--- src/main/java/cat/itb/m13/toysandsahre/model/entitats/Products.java
+++ src/main/java/cat/itb/m13/toysandsahre/model/entitats/Products.java
@@ -2,10 +2,7 @@ package cat.itb.m13.toysandsahre.model.entitats;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.Date;
 import java.util.Set;
 
@@ -15,16 +12,12 @@ public class Products {
     @Id
     @GeneratedValue
     int id;
-    Double price;
     String productName;
+    Double price;
     String productLocation;
     Long productDescription;
     Date dateCreated;
     String imageLink;
-
-    @ManyToMany
-    Set<Users> users;
-
 
 
     public Products(int id, Double price, String productName, String productLocation, Long productDescription, Date dateCreated, String imageLink) {
