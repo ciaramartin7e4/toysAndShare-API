536b8b719f22d706089171c33b7c4e3e9bfc84d6
sjo <joseph.duran.7e4@itb.cat>
2022-04-26 15:48:52 +0200

    API TOYSANDSHARE

--- src/main/java/cat/itb/m13/toysandsahre/ConfiguracioSeguretat.java
+++ src/main/java/cat/itb/m13/toysandsahre/ConfiguracioSeguretat.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -26,4 +28,23 @@ public class ConfiguracioSeguretat extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(elmeuUserDetailsService).passwordEncoder(xifrat);
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+//per poder accedir al h2-console
+                //  .authorizeRequests().antMatchers("/").permitAll().and()
+                //  .authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                // .and()
+                .csrf().disable()
+                // .headers().frameOptions().disable()
+                // .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
 }
