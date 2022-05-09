d1a1322343c0bf9e357c3a6c4f8eed7bfbcef031
ciara <ciara.martin.7e4@itb.cat>
2022-05-09 16:12:26 +0200

    commit

--- src/main/java/cat/itb/m13/toysandsahre/model/entitats/ProductoDTO.java
+++ src/main/java/cat/itb/m13/toysandsahre/model/entitats/ProductoDTO.java
@@ -3,12 +3,6 @@ package cat.itb.m13.toysandsahre.model.entitats;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-<<<<<<< HEAD
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-=======
->>>>>>> 2880763ad5986c080f17f05d8a8dedac489a9156
 import java.util.Date;
 @Data
 @NoArgsConstructor
@@ -20,15 +14,9 @@ public class ProductoDTO {
     String productDescription;
     Date dateCreated;
     String imageLink;
-<<<<<<< HEAD
     int usuariId;
 
     public ProductoDTO(int id, String productName, Double price, String productLocation, String productDescription, Date dateCreated, String imageLink, int usuariId) {
-=======
-    int usuari_id;
-
-    public ProductoDTO(int id, String productName, Double price, String productLocation, String productDescription, Date dateCreated, String imageLink, int usuari_id) {
->>>>>>> 2880763ad5986c080f17f05d8a8dedac489a9156
         this.id = id;
         this.productName = productName;
         this.price = price;
@@ -36,10 +24,5 @@ public class ProductoDTO {
         this.productDescription = productDescription;
         this.dateCreated = dateCreated;
         this.imageLink = imageLink;
-<<<<<<< HEAD
-        this.usuariId = usuariId;
-=======
-        this.usuari_id = usuari_id;
->>>>>>> 2880763ad5986c080f17f05d8a8dedac489a9156
     }
 }
